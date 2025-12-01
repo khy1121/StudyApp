7fa5c80b69b5f68abc07366e301b04c0c9e10bf3
khy1121 <rlagjsdud3@hansung.ac.kr>
2025-12-02 02:07:04 +0900

    워크플로삭제

--- build.sh
+++ build.sh
@@ -1,12 +1,1 @@
-#!/bin/sh
-set -e
 
-rm -rf output
-mkdir -p output
-
-# 현재 디렉터리의 최상위 항목들 중에서
-# output, .git 은 제외하고 모두 output/ 으로 복사
-find . -mindepth 1 -maxdepth 1 \
-  ! -name 'output' \
-  ! -name '.git' \
-  -exec cp -R {} output/ \;
